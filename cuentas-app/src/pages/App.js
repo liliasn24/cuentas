65abc253019000026d57791664ba73b0f7e47bd0
= <salaslilia00@gmail.com>
2021-11-24 23:09:00 -0500

    returned data from my database

--- cuentas-app/src/pages/App.js
+++ cuentas-app/src/pages/App.js
@@ -1,5 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App(props) {
-	return <div className="AppPage">This is the {props.page} page</div>;
+	const [cuentas, setCuentas] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('/api/cuentas');
+				const data = await response.json();
+				setCuentas(data);
+			} catch (error) {}
+		})();
+	}, []);
+
+	return (
+		<div className="AppPage">
+			This is the {props.page} page
+			<ul>
+				{cuentas.map(cuenta => {
+					return (
+						<li key={cuenta._id}>
+							This is the first cuenta: In {cuenta.for} we spent{cuenta.amount}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
